1dd23112ecd1e6041eec63451481557a31c85b25
DESKTOP-MRC2IME\A-23 <DESKTOP-MRC2IME\A-23>
2022-08-09 12:05:15 +0800

    make spawn

--- Assets/Script/EnemyContrpller.cs
+++ Assets/Script/EnemyContrpller.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class EnemyContrpller : MonoBehaviour
 {
-    public float speed;
+    public Vector2 Speed;
+    public float Forward;
+    public Rigidbody2D Enemy;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Enemy = GetComponent<Rigidbody2D>();
+        Enemy.velocity = Speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position.y -= speed * Time.deltaTime;
+        if (transform.position.y <= -6.0f)
+        {
+            Debug.Log("destroy");
+            Destroy(Enemy);
+        }
     }
+
+
+
+
 }
--- Assets/Script/SpawnController.cs
+++ Assets/Script/SpawnController.cs
@@ -1,18 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnController : MonoBehaviour
 {
+    public List<GameObject> ListEnemyTemplate;
+    private List<GameObject> EnemyList;
+    private float timerSpawn;
+    public int spawnInterval, maxSpawn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        EnemyList = new List<GameObject>;
+        timerSpawn = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        SpawnEnemy();
+        //StartCoroutine(SpawnInit());
+    }
+
+    private void SpawnEnemy()
+    {
+        timerSpawn += Time.deltaTime;
+
+        if (timerSpawn > spawnInterval )
+        {
+            if (EnemyList.Count < maxSpawn)
+            {
+                GenerateRandom();
+            }
+
+            spawnInterval
+        }
+    }
+
+    private void GenerateRandom()
+    {
         
     }
+
+    /*IEnumerator SpawnInit()
+    {
+        float spawnX = Random.Range(-6, 6);
+        Vector3 spawnPosition = new Vector3(spawnX, 6, 0);
+
+        Instantiate(Enemy, spawnPosition, Quaternion.identity);
+
+        yield return new WaitForSeconds(10);
+
+    }*/
 }
